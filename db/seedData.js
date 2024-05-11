5aafb9273891cf461140aa56b5d7a0d7a783f882
Sequoia Nevae <sequoianevae@Sequoias-iMac.lan>
2024-05-11 00:37:52 -0500

    fixed comma needed in seedData

--- db/seedData.js
+++ db/seedData.js
@@ -330,7 +330,7 @@ async function createInitialReviews() {
         rating: 3,
         comment: "Decent movie, could have been better.",
         review_date: "2021-01-03"
-      }
+      },
       {
         movie_id: 1,
         user_id: 1,
